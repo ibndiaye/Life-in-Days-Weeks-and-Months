9f2125e363683ab813efabde1448671d09fe2273
Ibrahima <63436162+ibndiaye@users.noreply.github.com>
2021-04-16 11:05:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+life_time= 90
+new_age=int(age)
+days_left=(life_time-new_age)*365
+# print(days_left)
+
+weeks_left=(life_time-new_age)*52
+# print(weeks_left)
+
+months_left= (life_time-new_age)*12
+# print(months_left)
+
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left if you were to live till 90.")
+
+
+
+
+
+
+
+
